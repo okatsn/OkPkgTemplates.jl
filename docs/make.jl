d6395fa706c8946a81e717bba4dcfcd985c498d3
okatsn <okatsn@gmail.com>
2022-12-14 19:01:50 +0800

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.29

---
+++ docs/make.jl
@@ -0,0 +1,25 @@
+using OkPkgTemplates
+using Documenter
+
+DocMeta.setdocmeta!(OkPkgTemplates, :DocTestSetup, :(using OkPkgTemplates); recursive=true)
+
+makedocs(;
+    modules=[OkPkgTemplates],
+    authors="okatsn <okatsn@gmail.com> and contributors",
+    repo="https://github.com/okatsn/OkPkgTemplates.jl/blob/{commit}{path}#{line}",
+    sitename="OkPkgTemplates.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://okatsn.github.io/OkPkgTemplates.jl",
+        edit_link="main",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/okatsn/OkPkgTemplates.jl",
+    devbranch="main",
+)
---
+++ docs/src/index.md
@@ -0,0 +1,14 @@
+```@meta
+CurrentModule = OkPkgTemplates
+```
+
+# OkPkgTemplates
+
+Documentation for [OkPkgTemplates](https://github.com/okatsn/OkPkgTemplates.jl).
+
+```@index
+```
+
+```@autodocs
+Modules = [OkPkgTemplates]
+```
