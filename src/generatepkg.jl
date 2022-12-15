4612eb7f996de96cf4e5bdef1b28c6fb3a5af27a
okatsn <okatsn@gmail.com>
2022-12-15 12:09:35 +0800

    Set PkgTemplates.user_view successfully

--- src/generatepkg.jl
+++ src/generatepkg.jl
@@ -4,14 +4,14 @@ macro gen_pkg(yourpkgname::String)
     return quote
         t = OkPkgTemplates.template_001()
         function PkgTemplates.user_view(::Readme, ::Template, ::AbstractString)
-            Dict(
-                "TODAY" => "today",
+            default_readme_var = PkgTemplates.view(PLUGIN_README,t,$yourpkgname)
+            merge!(default_readme_var, Dict(
+                "TODAY" => today(),
                 "PKG" => "NOTHELLOWORLD"
-            )
+            ))
+            return default_readme_var
         end
+
         t($yourpkgname)
     end
 end
-
-
-#         default_readme_var = PkgTemplates.view(PLUGIN_README,t,yourpkgname)
