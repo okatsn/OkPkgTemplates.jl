78fdfaaeb9c525b534b0709da5294cbb3e2018ab
okatsn <okatsn@gmail.com>
2022-12-15 10:06:20 +0800

    Organize the plugin settings
    It shouldn't change the output

--- src/mypkgtemplates.jl
+++ src/mypkgtemplates.jl
@@ -32,6 +32,43 @@ function ok_pkg_template(yourpkgname::String; destination="", julia_ver = v"1.6"
         destination = Pkg.devdir()
     end
 
+    # # Set up Plugins
+    # Readme
+    # - see also PkgTemplates/src/plugins/readme.jl
+    rdme = PkgTemplates.Readme(; file=mypkgtemplate_dir("README.md"), destination="README.md")
+
+    # TagBot
+    # - see PkgTemplates/src/plugins/tagbot.jl
+    # - `changelog` is modified from [default changelog template](https://github.com/JuliaRegistries/TagBot/blob/master/action.yml) with extra `${{github.event.head_commit.message}}` (for get the commit message).
+    # - If your registry is public, this is all you need to do. For more information, see [here](https://github.com/JuliaRegistries/TagBot#custom-registries)
+    tgbt = TagBot(;registry="okatsn/OkRegistry",
+            changelog = """
+            ## {{ package }} {{ version }}
+            \${{github.event.head_commit.message}}
+            {% if previous_release %}
+            [Diff since {{ previous_release }}]({{ compare_url }})
+            {% endif %}
+            {% if custom %}
+            {{ custom }}
+            {% endif %}
+            {% if issues %}
+            **Closed issues:**
+            {% for issue in issues %}
+            - {{ issue.title }} (#{{ issue.number }})
+            {% endfor %}
+            {% endif %}
+            {% if pulls %}
+            **Merged pull requests:**
+            {% for pull in pulls %}
+            - {{ pull.title }} (#{{ pull.number }}) (@{{ pull.author.username }})
+            {% endfor %}
+            {% endif %}
+            """,
+            file = mypkgtemplate_dir("github", "workflows", "TagBot.yml"))
+
+    # Test
+    tst = Tests(; file=mypkgtemplate_dir("test","runtests.jl")) # see PkgTemplates/src/plugins/tests.jl
+
     t = Template(;
     user=username,
     dir=destination,
@@ -41,36 +78,9 @@ function ok_pkg_template(yourpkgname::String; destination="", julia_ver = v"1.6"
         GitHubActions(;file = mypkgtemplate_dir("github", "workflows", "CI.yml")), # see PkgTemplates/src/plugins/ci.jl
         Codecov(), # https://about.codecov.io/
         Documenter{GitHubActions}(),
-        PkgTemplates.Readme(; file=mypkgtemplate_dir("README.md"), destination="README.md"), # see PkgTemplates/src/plugins/readme.jl
-        TagBot(;registry="okatsn/OkRegistry",
-                # If your registry is public, this is all you need to do.
-                # For more information, see [here](https://github.com/JuliaRegistries/TagBot#custom-registries)
-                changelog = """
-                ## {{ package }} {{ version }}
-                \${{github.event.head_commit.message}}
-                {% if previous_release %}
-                [Diff since {{ previous_release }}]({{ compare_url }})
-                {% endif %}
-                {% if custom %}
-                {{ custom }}
-                {% endif %}
-                {% if issues %}
-                **Closed issues:**
-                {% for issue in issues %}
-                - {{ issue.title }} (#{{ issue.number }})
-                {% endfor %}
-                {% endif %}
-                {% if pulls %}
-                **Merged pull requests:**
-                {% for pull in pulls %}
-                - {{ pull.title }} (#{{ pull.number }}) (@{{ pull.author.username }})
-                {% endfor %}
-                {% endif %}
-                """,
-                file = mypkgtemplate_dir("github", "workflows", "TagBot.yml"),
-            ), # see PkgTemplates/src/plugins/tagbot.jl
-               # It use the template TagBot.yml in PkgTemplates directly, the mypkgtemplate/github/workflows/TagBot.yml is just for reference and can be deleted. `changelog` is modified from [default changelog template](https://github.com/JuliaRegistries/TagBot/blob/master/action.yml) with extra `${{github.event.head_commit.message}}`.
-        Tests(; file=mypkgtemplate_dir("test","runtests.jl")) # see PkgTemplates/src/plugins/tests.jl
+        rdme,
+        tgbt,
+        tst
     ],
 
     ) # https://www.juliabloggers.com/tips-and-tricks-to-register-your-first-julia-package/
