5fab261df8e33fc8275585c77f0438b24c98f25b
okatsn <okatsn@gmail.com>
2023-02-02 14:23:15 +0800

    Fix TagBot and unlink its response to register
    - Delete the inclusion of github.event.head_commit.message that is the origin of unexpected TagBot error.
    - Basically restore TagBot.yml to default, but with `tags-ignore:  - '*-DEV'`

--- src/myplugins.jl
+++ src/myplugins.jl
@@ -7,28 +7,28 @@ PLUGIN_README() = PkgTemplates.Readme(; file=mypkgtemplate_dir("README.md"), des
 # - `changelog` is modified from [default changelog template](https://github.com/JuliaRegistries/TagBot/blob/master/action.yml) with extra `${{github.event.head_commit.message}}` (for get the commit message).
 # - If your registry is public, this is all you need to do. For more information, see [here](https://github.com/JuliaRegistries/TagBot#custom-registries)
 PLUGIN_TAGBOT() = TagBot(;registry="okatsn/OkRegistry",
-        changelog = """
-        ## {{ package }} {{ version }}
-        \${{ github.event.head_commit.message }}
-        {% if previous_release %}
-        [Diff since {{ previous_release }}]({{ compare_url }})
-        {% endif %}
-        {% if custom %}
-        {{ custom }}
-        {% endif %}
-        {% if issues %}
-        **Closed issues:**
-        {% for issue in issues %}
-        - {{ issue.title }} (#{{ issue.number }})
-        {% endfor %}
-        {% endif %}
-        {% if pulls %}
-        **Merged pull requests:**
-        {% for pull in pulls %}
-        - {{ pull.title }} (#{{ pull.number }}) (@{{ pull.author.username }})
-        {% endfor %}
-        {% endif %}
-        """,
+        # changelog = """
+        # ## {{ package }} {{ version }}
+        # \${{ github.event.head_commit.message }}
+        # {% if previous_release %}
+        # [Diff since {{ previous_release }}]({{ compare_url }})
+        # {% endif %}
+        # {% if custom %}
+        # {{ custom }}
+        # {% endif %}
+        # {% if issues %}
+        # **Closed issues:**
+        # {% for issue in issues %}
+        # - {{ issue.title }} (#{{ issue.number }})
+        # {% endfor %}
+        # {% endif %}
+        # {% if pulls %}
+        # **Merged pull requests:**
+        # {% for pull in pulls %}
+        # - {{ pull.title }} (#{{ pull.number }}) (@{{ pull.author.username }})
+        # {% endfor %}
+        # {% endif %}
+        # """,
         file = mypkgtemplate_dir("github", "workflows", "TagBot.yml"))
 
 # Test
